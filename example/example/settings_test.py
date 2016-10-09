f8fc3f088af57dda1d97b791d44ff479f6305360
pgmgonta <pgm.gonta@gmail.com>
2016-10-09 22:24:26 +0900

    add test settings

---
+++ example/example/settings_test.py
@@ -0,0 +1,25 @@
+# settings_test.py
+
+from example.settings import *
+import os
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+
+COVERAGE_REPORT_HTML_OUTPUT_DIR = '.cover'
+
+NOSE_ARGS = [
+    '--with-xunit',
+    '--with-coverage',
+    '--cover-xml',
+    '--cover-html',
+    '--cover-package=todo', 
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'test.db'),
+    }
+}
+
+
