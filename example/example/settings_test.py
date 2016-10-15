4241cb2b4fc25db590345172e4885c13d5621327
pgmgonta <pgm.gonta@gmail.com>
2016-10-15 21:20:00 +0900

    add django-jenkins

--- example/example/settings_test.py
+++ example/example/settings_test.py
@@ -3,9 +3,24 @@
 from example.settings import *
 import os
 
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+#TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+TEST_RUNNER = 'django_jenkins.runner.CITestSuiteRunner'
 
 COVERAGE_REPORT_HTML_OUTPUT_DIR = '.cover'
+
+# django-jenkins
+INSTALLED_APPS += (
+    'django_jenkins',
+)
+JENKINS_TASKS = (
+    'django_jenkins.tasks.run_pylint',
+    'django_jenkins.tasks.run_pep8',
+ #   'django_jenkins.tasks.run_pyflakes',
+ #   'django_jenkins.tasks.run_flake8',
+)
+PROJECT_APPS = (
+    'todo',
+)
 
 NOSE_ARGS = [
     '--with-xunit',
